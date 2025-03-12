e0de4dd8b3958e9c582a2711579786671c10676f
Anatoli <PanasAA@web.de>
2025-03-12 14:31:32 +0100

    add

---
+++ ha_1/test_simple_math.py
@@ -0,0 +1,24 @@
+import pytest
+from simple_math import SimpleMath
+
+@pytest.fixture
+def simplemath():
+    return SimpleMath()
+
+@pytest.mark.parametrize("a, exp", [
+    (2, 4),
+    (0, 0),
+    (1, 1),
+    (-2, 4),
+])
+def test_square_po_muns(a, exp, simplemath):
+    assert simplemath.square(a)  == exp
+
+@pytest.mark.parametrize("a, exp", [
+    (2, 8),
+    (0, 0),
+    (1, 1),
+    (-2, -8),
+])
+def test_cube_po_muns(a, exp, simplemath):
+    assert simplemath.cube(a)  == exp

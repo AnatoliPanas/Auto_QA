5e9b2839d582ec65923c4743938e1915f74c1c15
Anatoli <PanasAA@web.de>
2025-04-02 14:48:27 +0200

    add

--- ha_4/test_ha_4.py
+++ ha_4/test_ha_4.py
@@ -1,8 +1,6 @@
 import time
-
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 def test_button_new_name(driver):
     driver.get("http://uitestingplayground.com/textinput")
